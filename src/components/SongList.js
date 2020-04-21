1d6c6aaf9c4cf3f0759433a5dae190a0ac70ccaf
djghis <djghis@hotmail.com>
2020-04-21 18:24:44 +0100

    image added

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -4,19 +4,20 @@ import React from 'react';
 
 const SongList = (props) => {
 
-  const topSongs = props.songs.map((song, index) => {
+  const top20Songs = props.songs.map((song, index) => {
     return <ul key={index}>
     <li>Number {index +1}</li>
     <p>{song["im:name"].label}</p>
     <p>by: {song["im:artist"].label}</p>
+    <img src={song["im:image"][1].label} alt="Album cover image"/>
     <br></br>
     </ul>
   })
 
   return (
-    <ol>
-      {topSongs}
-    </ol>
+    <ul>
+      {top20Songs}
+    </ul>
   )
 }
 
