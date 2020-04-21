b0702fa08dc961f5526fef739db34eb3d97682c1
djghis <djghis@hotmail.com>
2020-04-21 16:37:59 +0100

    Api fetched

--- src/containers/Top20Container.js
+++ src/containers/Top20Container.js
@@ -8,6 +8,14 @@ class Top20Container extends React.Component {
             songs: []
          }
     }
+
+    componentDidMount(){
+        const url = 'https://itunes.apple.com/gb/rss/topsongs/limit=20/json';
+
+        fetch(url)
+        .then(res => res.json())
+        .then(songs => this.setState({songs: songs.feed.entry}))
+    }
     render() { 
         return (
             <div>
